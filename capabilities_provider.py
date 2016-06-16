e2916bde6e18fef500e32ae0449eab20d90971e6
Stanisław Jankowski <staszek@man.poznan.pl>
2016-06-16 13:55:31 +0200

    Config driven approach. No CEPH communication

--- capabilities_provider.py
+++ capabilities_provider.py
@@ -1,21 +1,70 @@
 import json
+import configparser
+import ast
+Config = None
+
 
 class profile:
-    def __init__(self, name, allowed_profiles, metadata):
+
+    def get_metadata(self):
+        metadata = {}
+
+        cdmi_latency = self.get_cdmi_latency()
+        metadata["cdmi_latency"] = str(cdmi_latency)
+
+        print(type(self.get_cdmi_geographic_placement()))
+        cdmi_geographic_placement = self.get_cdmi_geographic_placement()
+        metadata["cdmi_geographic_placement"] = cdmi_geographic_placement
+
+        cdmi_data_redundancy = self.get_cdmi_data_redundancy()
+        metadata["cdmi_data_redundancy"] = str(cdmi_data_redundancy)
+
+        return metadata
+
+    def get_cdmi_latency(self):
+        global Config
+        return Config[self.name]["cdmi_latency"]
+
+    def get_cdmi_data_redundancy(self):
+        global Config
+        return Config[self.name]["cdmi_data_redundancy"]
+
+    def get_cdmi_geographic_placement(self):
+        global Config
+        string = Config[self.name]["cdmi_geographic_placement"]
+        return [chunk.strip(None) for chunk in string.split(',')]
+
+    def __init__(self, name, values):
         self.name = name
-        self.type = "container"
-        self.allowed_profiles = allowed_profiles
-        self.metadata = metadata
+        self.type = values["type"]
+        self.allowed_profiles = []
+        self.metadata = self.get_metadata()
+
+
+def read_config():
+    global Config
+    Config = configparser.ConfigParser()
+    Config.read("profile_config.ini")
+
+
+def get_profiles_names():
+    global Config
+    return Config.sections()
+
+
+def get_profiles():
+    global Config
+    profiles = []
+    profiles_names = get_profiles_names()
+    for p in profiles_names:
+        single_profile = profile(p, Config[p])
+        profiles.append(single_profile.__dict__)
+    return profiles
+
 
 def main():
-    profiles = []
-    metadata1 = {"cdmi_latency": "300", "cdmi_geographic_placement": ["PL","GB"], "cdmi_data_redundancy": "10"}
-    metadata2 = {"cdmi_latency": "1", "cdmi_geographic_placement": ["RU","FR"], "cdmi_data_redundancy": "2"}
-    profile1 = profile("profil1", [], metadata1)
-    profile2 = profile("profil2", [], metadata2)
-    profiles.append(profile1.__dict__)
-    profiles.append(profile2.__dict__)
-
+    read_config()
+    profiles = get_profiles()
     print(json.dumps(profiles))
 
 if __name__ == "__main__":
