08aec0e419f3598865c71b098b14c31875a59cf2
Stanisław Jankowski <staszek@man.poznan.pl>
2016-06-16 12:10:49 +0200

    First version with probably correct output.

--- capabilities_provider.py
+++ capabilities_provider.py
@@ -9,15 +9,14 @@ class profile:
 
 def main():
     profiles = []
-    metadata1 = {"latency": 300, "geographic_placement": ["PL","GB"], "data_redundancy": 10}
-    metadata2 = {"latency": 1, "geographic_placement": ["RU","FR"], "data_redundancy": 2}
+    metadata1 = {"cdmi_latency": "300", "cdmi_geographic_placement": ["PL","GB"], "cdmi_data_redundancy": "10"}
+    metadata2 = {"cdmi_latency": "1", "cdmi_geographic_placement": ["RU","FR"], "cdmi_data_redundancy": "2"}
     profile1 = profile("profil1", [], metadata1)
     profile2 = profile("profil2", [], metadata2)
     profiles.append(profile1.__dict__)
     profiles.append(profile2.__dict__)
 
     print(json.dumps(profiles))
-    print("OK")
 
 if __name__ == "__main__":
     main()
