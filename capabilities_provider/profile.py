98a81d0dd034620b6194b694a3277df4ff4a9a45
Stanisław Jankowski <staszek@man.poznan.pl>
2016-06-23 14:50:51 +0200

    fixes

--- capabilities_provider/profile.py
+++ capabilities_provider/profile.py
@@ -7,10 +7,10 @@ class Profile:
     def __get_metadata(self, values):
         metadata = {}
 
-        cdmi_latency = values[self.name]["cdmi_latency"]
+        cdmi_latency = values["cdmi_latency"]
         metadata["cdmi_latency"] = str(cdmi_latency)
 
-        cdmi_geographic_placement = self.__get_cdmi_geographic_placement()
+        cdmi_geographic_placement = self.__get_cdmi_geographic_placement(values)
         metadata["cdmi_geographic_placement"] = cdmi_geographic_placement
 
         cdmi_data_redundancy = self.__get_cdmi_data_redundancy(values)
@@ -23,8 +23,8 @@ class Profile:
         output = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         return int(output.stdout.readline()[6:-1])
 
-    def __get_cdmi_geographic_placement(self):
-        string = self.values[self.name]["cdmi_geographic_placement"]
+    def __get_cdmi_geographic_placement(self, values):
+        string = values["cdmi_geographic_placement"]
         return [chunk.strip(None) for chunk in string.split(',')]
 
     def __init__(self, name, values):
