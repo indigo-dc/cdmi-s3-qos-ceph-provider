f365b6d87574d1bf8bc911511946e4102b6cc30c
Stanisław Jankowski <staszek@man.poznan.pl>
2016-06-23 14:41:43 +0200

    profile class fixes

--- capabilities_provider/profile.py
+++ capabilities_provider/profile.py
@@ -19,22 +19,29 @@ class Profile:
         return metadata
 
     def get_cdmi_latency(self):
-        global Config
-        return Config[self.name]["cdmi_latency"]
+        return self.latency
 
     def get_cdmi_data_redundancy(self):
+        return self.data_redundancy
+
+    def __get_cdmi_data_redundancy(self, values):
         command = "ceph osd pool get "+self.pools[0]+" size"
         output = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         return int(output.stdout.readline()[6:-1])
 
     def get_cdmi_geographic_placement(self):
-        global Config
-        string = Config[self.name]["cdmi_geographic_placement"]
+        return self.geographic_placement
+
+    def __get_cdmi_geographic_placement(self):
+        string = self.values[self.name]["cdmi_geographic_placement"]
         return [chunk.strip(None) for chunk in string.split(',')]
 
     def __init__(self, name, values):
         self.name = name
-        self.pools = ast.literal_eval(Config.get(self.name, "pools"))
+        self.pools = ast.literal_eval(values.get("pools"))
         self.type = values["type"]
         self.allowed_profiles = []
         self.metadata = self.get_metadata()
+        self.latency = self.values[self.name]["cdmi_latency"]
+        self.data_redundancy = self.__get_cdmi_data_redundancy(values)
+        self.geographic_placement = self.__get_cdmi_geographic_placement()
